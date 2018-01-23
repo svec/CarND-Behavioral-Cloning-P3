917b424313f7be690e9ff180af98062ce1886e19
Christopher Svec <csvec@irobot.com>
2018-01-23 15:47:08 -0500

    added test data and updated nogen-nn-norand.py

--- nogen-nn-norand.py
+++ nogen-nn-norand.py
@@ -20,22 +20,15 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('data_dirs', 'car-data/run-0', "directories of car data")
 flags.DEFINE_string('model_file_output', 'model.h5', "output model file name")
 flags.DEFINE_integer('epochs', 1, "# epochs")
-flags.DEFINE_float('dropout_rate', 0.5, "dropout rate = % to drop")
-flags.DEFINE_float('learning_rate', 0.001, "learning rate")
-flags.DEFINE_string('model_type', 'nvidia', "NN model name")
 
 
 def read_csv(dirname):
     print("Reading directory:", dirname)
     lines = []
-    hacked_count = 0
     with open(dirname + "/driving_log.csv") as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
             lines.append(line)
-            if hacked_count > 4:
-                break
-            hacked_count = hacked_count + 1
 
     images = []
     measurements = []
@@ -54,7 +47,7 @@ def read_csv(dirname):
             continue
         # cv2.imread() reads in as BGR by default. Convert to RGB for our own sanity.
         image = cv2.imread(rel_path_image_filename)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         images.append(image)
         measurement = float(line[3])
         measurements.append(measurement)
@@ -81,32 +74,12 @@ def main(_):
     image_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3])
     print("image shape:", image_shape) # should be: image shape: (160, 320, 3)
 
-    model_type = FLAGS.model_type
-    dropout_rate = FLAGS.dropout_rate
-    learning_rate = FLAGS.learning_rate
-
     model = Sequential()
-
-    # Poor man's normalization.
-    # Image data starts at 0-255 (for RGB or YUV).
-    # /255 makes it 0-1, then subtracting 0.5 centers it at 0.
-    model.add(core.Lambda(lambda x: x/255.0 - 0.5, input_shape=image_shape))
-    # Crops (removes) these pixels:
-    # - top 65 pixels
-    # - bottom 25 pixels
-    # - 0 from the left or right
-    model.add(Cropping2D(cropping=((65,25), (1,1))))
-
-    print("Using model_type:", model_type)
-    print("Using dropout rate:", dropout_rate)
-    print("Using learning rate", learning_rate)
-
-    model.add(Flatten())
+    model.add(Flatten(input_shape=image_shape))
     model.add(Dense(1))
     #model.summary()
 
-    adam = optimizers.Adam(lr=learning_rate)
-    model.compile(loss='mse', optimizer=adam)
+    model.compile(loss='mse', optimizer='adam')
 
     model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=FLAGS.epochs)
     
